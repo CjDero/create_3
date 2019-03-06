d4cb814f72d09561d514da2e64e23372aa9cc713
CjDero <22333420+CjDero@users.noreply.github.com>
2019-03-06 15:45:43 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+let app = new Vue({
+  el: '#app',
+  data: {
+    message: '',
+    clickedbutt: false,
+    loading: true,
+    savedMessages: [],
+  },
+  methods: {
+    async getAdvice() {
+      try {
+        this.clickedbutt = true;
+        this.loading = true;
+        const response = await axios.get('https://api.adviceslip.com/advice');
+        this.loading = false;
+        console.log(response);
+        this.message = response.data.slip.advice;
+      } catch (error) {
+        console.log(error)
+        this.message = "Error finding advice";
+      }
+    },
+    async getQuote() {
+      try {
+        this.clickedbutt = true;
+        this.loading = true;
+        let urlTemp = 'https://api.kanye.rest';
+        const url = proxyurl + urlTemp;
+        const response = await axios.get(url);
+        console.log(response);
+        this.loading = false;
+        this.message = response.data.quote + ' -Kanye Best       https://www.programmableweb.com/api/ermahgerd-translator';
+      } catch (error) {
+        console.log(error)
+        this.message = "Error finding quote";
+      }
+    },
+    async getInsult() {
+      try {
+        this.clickedbutt = true;
+        this.loading = true;
+        let urlTemp = 'https://evilinsult.com/generate_insult.php?lang=en&type=json';
+        const url = proxyurl + urlTemp;
+        const response = await axios.get(url);
+        this.loading = false;
+        console.log("what");
+        console.log(response);
+        this.message = response.data.insult;
+      } catch (error) {
+        console.log("error");
+        console.log(error)
+        this.message = "Error finding quote";
+      }
+    },
+    saveMessage() {
+      this.savedMessages.push(this.message);
+    },
+  },
+});
